a91e9741b080844aa6ee021218a180b8d1e25303
Facundo Luparello <facundoluparello3@gmail.com>
2023-04-22 13:12:45 -0300

    jvsc

---
+++ primer.js
@@ -0,0 +1,4 @@
+function calculateLoan() {
+    // Obtenemos los valores del formulario
+    const loanAmount = parseFloat(document.getElementById("loanAmount").value);
+    const interestRate = parseFloat(document.getElementById("interestRate").value);}
